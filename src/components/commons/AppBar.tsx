3807a23fff8427600654e17369e7495b32b08103
david <dvdprr6@gmail.com>
2022-02-21 16:35:40 -0500

    remove settings menu

--- src/components/commons/AppBar.tsx
+++ src/components/commons/AppBar.tsx
@@ -1,42 +1,12 @@
-import React, { FC, useState, useCallback } from 'react'
+import React from 'react'
 import {
   Text,
-  TopNavigation,
-  Icon,
-  TopNavigationAction,
-  OverflowMenu,
-  MenuItem,
+  TopNavigation
 } from '@ui-kitten/components'
-import { TScreenProps, SETTINGS_SCREEN } from '../types'
 
-const AppBar: FC<TScreenProps> = (props) => {
-  const { navigation } = props
-  const [visible, setVisible] = useState<boolean>(false)
-
-  const navigateToSettingsScreen = useCallback(() => {
-    navigation.navigate(SETTINGS_SCREEN)
-    setVisible(false)
-  }, [visible])
-
+const AppBar = () => {
   return (
-      <TopNavigation
-        title={() => <Text status={'info'}>Crab</Text>}
-        accessoryRight={() => (
-          <OverflowMenu
-            anchor={() => (
-              <TopNavigationAction
-                icon={props => <Icon {...props} name={'menu-outline'} />}
-                onPress={() => setVisible(true)}
-              />
-            )}
-            visible={visible}
-            onSelect={() => navigateToSettingsScreen()}
-            onBackdropPress={() => setVisible(false)}
-          >
-            <MenuItem title={'Settings'} />
-          </OverflowMenu>
-        )}
-      />
+      <TopNavigation title={() => <Text status={'info'}>Crab</Text>} />
   )
 }
 
