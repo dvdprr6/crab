dee64755d224f4c5d2b9bc150b4748a6d603fb0d
david <dvdprr6@gmail.com>
2022-03-06 14:30:48 -0500

    List wrap for details page

--- src/components/details/Details.tsx
+++ src/components/details/Details.tsx
@@ -1,9 +1,10 @@
 import React, { FC } from 'react'
-import { Text, Layout, Card, List, ListItem, Icon } from '@ui-kitten/components'
+import { Text, Layout, Card, List, ListItem, Icon, Button } from "@ui-kitten/components";
 import { PieChart } from 'react-native-chart-kit'
 import { StyleSheet, Dimensions } from 'react-native'
 import { TScreenProps } from '../types'
 import { EXPENSE } from '@crab-utils'
+import moment from "moment";
 
 const chartConfig = {
   backgroundGradientFrom: '#1E2923',
@@ -19,6 +20,10 @@ const styles = StyleSheet.create({
   card: {
     margin: 2
   },
+  listContainer: {
+    paddingHorizontal: 8,
+    paddingVertical: 4
+  },
   status: {
     flexDirection: 'row',
     justifyContent: 'space-between'
@@ -27,7 +32,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between'
   },
-  highestItem: {
+  main: {
     flexDirection: 'row',
     justifyContent: 'space-between'
   }
@@ -38,52 +43,105 @@ const Details: FC<TScreenProps> = (props) => {
 
   return (
     <Layout style={{ flex: 1 }}>
-      <Card style={styles.card} disabled>
-        <Layout style={styles.status}>
-          <Text>Status</Text>
-          <Text>{route.params?.status}</Text>
-        </Layout>
-      </Card>
-      <Card style={styles.card} disabled>
-        <Layout style={styles.total}>
-          <Text>Total Month Revenue</Text>
-          <Text>{new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(route.params?.revenue || 0)}</Text>
-        </Layout>
-        <Layout style={styles.total}>
-          <Text>Total Month Expenses</Text>
-          <Text>{new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(route.params?.expenses || 0)}</Text>
-        </Layout>
-        <Layout style={styles.total}>
-          <Text>Total Month Savings</Text>
-          <Text>{new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(route.params?.savings || 0)}</Text>
-        </Layout>
-      </Card>
-      <Card style={styles.card} disabled>
-        <PieChart
-          data={route.params?.chartData || []}
-          width={Dimensions.get('window').width}
-          height={100}
-          chartConfig={chartConfig}
-          accessor={'population'}
-          backgroundColor={'transparent'}
-          paddingLeft={'15'}
-          center={[10, 10]}
-        />
-      </Card>
-      <Card style={{ flex: 1 }} disabled>
+      <Layout>
         <List
-          data={route.params?.items || []}
-          renderItem={({ item, index }) => (
-            <ListItem
-              title={new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(item.amount)}
-              description={item.itemName}
-              accessoryLeft={(props) => (
-                item.itemType === EXPENSE ? <Icon {...props} name={'arrow-circle-down'} /> : <Icon {...props} name={'arrow-circle-up'} />
-              )}
-            />
+          contentContainerStyle={styles.listContainer}
+          data={[0]}
+          renderItem={() => (
+            <Layout>
+              <Card style={styles.card} disabled>
+                <Layout style={styles.status}>
+                  <Text>Status</Text>
+                  <Text>{route.params?.status}</Text>
+                </Layout>
+              </Card>
+            </Layout>
           )}
         />
-      </Card>
+      </Layout>
+      <Layout>
+        <List
+          contentContainerStyle={styles.listContainer}
+          data={[0]}
+          renderItem={() => (
+            <Layout>
+              <Card style={styles.card} disabled>
+                <Layout style={styles.total}>
+                  <Text>Total Month Revenue</Text>
+                  <Text>{new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(route.params?.revenue || 0)}</Text>
+                </Layout>
+                <Layout style={styles.total}>
+                  <Text>Total Month Expenses</Text>
+                  <Text>{new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(route.params?.expenses || 0)}</Text>
+                </Layout>
+                <Layout style={styles.total}>
+                  <Text>Total Month Savings</Text>
+                  <Text>{new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(route.params?.savings || 0)}</Text>
+                </Layout>
+              </Card>
+            </Layout>
+          )}
+        />
+      </Layout>
+      <Layout>
+        <List
+          contentContainerStyle={styles.listContainer}
+          data={[0]}
+          renderItem={() => (
+            <Layout>
+              <Card style={styles.card} disabled>
+                <PieChart
+                  data={route.params?.chartData || []}
+                  width={Dimensions.get('window').width}
+                  height={100}
+                  chartConfig={chartConfig}
+                  accessor={'population'}
+                  backgroundColor={'transparent'}
+                  paddingLeft={'0'}
+                  center={[10, 10]}
+                />
+              </Card>
+            </Layout>
+          )}
+        />
+      </Layout>
+      <Layout style={{ flex: 1 }}>
+        <List
+          contentContainerStyle={styles.listContainer}
+          data={route.params?.items || []}
+          renderItem={props => (
+            <Layout>
+              <Card
+                disabled
+                key={props.item.id}
+                style={styles.card}
+                status={props.item.itemType === EXPENSE ? 'danger' : 'success'}
+              >
+                <Layout style={styles.main}>
+                  <Text>Item Name</Text>
+                  <Text>{props.item.itemName}</Text>
+                </Layout>
+                <Layout style={styles.main}>
+                  <Text>Amount</Text>
+                  <Text>{new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(props.item.amount)}</Text>
+                </Layout>
+                <Layout style={styles.main}>
+                  <Text>Date</Text>
+                  <Text>{moment(props.item.createDate).format('LLL')}</Text>
+                </Layout>
+                <Layout style={styles.main}>
+                  <Text>Type</Text>
+                  <Text>{props.item.itemType}</Text>
+                </Layout>
+                <Layout style={styles.main}>
+                  <Text>Is Re-occurring?</Text>
+                  <Text>{props.item.recurring ? 'Yes' : 'No'}</Text>
+                </Layout>
+              </Card>
+            </Layout>
+          )}
+        />
+      </Layout>
     </Layout>
   )
 }
